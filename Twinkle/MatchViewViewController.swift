e3903225407eb5854cb0f55ec6fcc0b22c10e18a
Lynette Nguyen <lynettenguyen0909@gmail.com>
2020-02-02 04:19:57 -0800

    Added swift files to all TableViewControllers

---
+++ Twinkle/MatchViewViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MatchViewViewController.swift
+//  Twinkle
+//
+//  Created by Lynette Nguyen on 2/2/20.
+//  Copyright © 2020 Twinkle. All rights reserved.
+//
+
+import UIKit
+
+class MatchViewViewController: UIViewController {
+
+    @IBOutlet weak var matchImage: UIImageView!
+    @IBOutlet weak var matchName: UILabel!
+    @IBOutlet weak var matchSun: UILabel!
+    @IBOutlet weak var matchMoon: UILabel!
+    @IBOutlet weak var matchRising: UILabel!
+    @IBOutlet weak var matchBio: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
