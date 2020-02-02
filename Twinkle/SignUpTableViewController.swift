c79991f7530e3e53369e2bce30e832bd0c11c42b
Lily Pham <phamlily99@gmail.com>
2020-02-02 00:42:27 -0800

    Include GMS API key and display autocomplete places screen

--- Twinkle/SignUpTableViewController.swift
+++ Twinkle/SignUpTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import GooglePlaces
 
 class SignUpTableViewController: UITableViewController {
     
@@ -27,6 +28,21 @@ class SignUpTableViewController: UITableViewController {
     }
     
     @IBAction func selectCity(_ sender: Any) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+
+        // Specify the place data types to return.
+        let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) |
+          UInt(GMSPlaceField.placeID.rawValue))!
+        autocompleteController.placeFields = fields
+
+        // Specify a filter.
+        let filter = GMSAutocompleteFilter()
+        filter.type = .city
+        autocompleteController.autocompleteFilter = filter
+
+        // Display the autocomplete view controller.
+        present(autocompleteController, animated: true, completion: nil)
     }
     
     @IBAction func saveProfile(_ sender: Any) {
@@ -71,4 +87,37 @@ class SignUpTableViewController: UITableViewController {
         return true
     }
     
+}
+
+
+extension SignUpTableViewController: GMSAutocompleteViewControllerDelegate {
+
+  // Handle the user's selection.
+  func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+    print("Place name: \(place.name)")
+    print("Place ID: \(place.placeID)")
+    dismiss(animated: true, completion: nil)
+    place.coordinate.latitude
+    place.coordinate.longitude
+  }
+
+  func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+    // TODO: handle the error.
+    print("Error: ", error.localizedDescription)
+  }
+
+  // User canceled the operation.
+  func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+    dismiss(animated: true, completion: nil)
+  }
+/*
+  // Turn the network activity indicator on and off again.
+  func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = true
+  }
+
+  func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+  }*/
+
 }
