9ab9c44539398d151472acdf40a2b9e7c937fe48
Lily Pham <phamlily99@gmail.com>
2020-02-01 17:10:37 -0800

    Create LoginViewController file

---
+++ Twinkle/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  Twinkle
+//
+//  Created by Lily Pham on 2/1/20.
+//  Copyright © 2020 Twinkle. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func login(_ sender: Any) {
+    }
+    
+
+}
